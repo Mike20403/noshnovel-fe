5327d0d821c82c61e1072c18154bc545e5f78c84
khuong.mai@nexpando <maikhuong2003@gmail.com>
2024-04-29 12:40:56 +0700

    add pagination

--- src/components/CardSearchList.tsx
+++ src/components/CardSearchList.tsx
@@ -1,6 +1,9 @@
 import { Novel } from '~/models/Novel.tsx';
-import { NovelCard } from '~/components/NovelCard.tsx';
 import { NovelSearchCard } from '~/components/NovelSearchCard.tsx';
+import ReactPaginate from 'react-paginate';
+import { useState } from 'react';
+import { CardSlider } from '~/components/CardSlider.tsx';
+import ReactDOM from 'react-dom/client';
 
 export type CardSearchListProps = {
   item: Novel[],
@@ -10,6 +13,14 @@ export type CardSearchListProps = {
 
 export const CardSearchList = (props: CardSearchListProps) => {
   const { item, totalRow = 1, totalCol = 1 } = props;
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = totalRow * totalCol;
+
+  const handlePageClick = (event: { selected: number }) => {
+    setCurrentPage(event.selected);
+  };
+
+  const displayedItems = item.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage);
 
   return (
     <div className="wrapper p-3 grid grid-cols-1 gap-4"> {/* Assumes 2 columns per row */}
@@ -17,9 +28,8 @@ export const CardSearchList = (props: CardSearchListProps) => {
         <div key={rowIndex} className="grid grid-cols-2 gap-4"> {/* Assumes 2 columns per row */}
           {Array.from({ length: totalCol }).map((_, colIndex) => {
             const index = rowIndex * totalCol + colIndex;
-            // if (index < item.length) {
-            //   const novel = item[index];
-            //
+            // if (index < displayedItems.length) {
+            //   const novel = displayedItems[index];
             // }
             return (
               <div key={index} className="col-span-1 p-3">
@@ -27,10 +37,25 @@ export const CardSearchList = (props: CardSearchListProps) => {
                 {<NovelSearchCard />}
               </div>
             );
-            return null;
+            // return null;
           })}
         </div>
       ))}
+      <ReactPaginate
+        className="w-full flex flex-row justify-center items-center space-x-5"
+        previousLabel="Previous"
+        nextLabel="Next"
+        pageCount={Math.ceil(10 / itemsPerPage)}
+        onPageChange={handlePageClick}
+        previousClassName="inline-block rounded-xl bg-app_primary p-2 text-white"
+        nextClassName="inline-block rounded-xl bg-app_primary p-2 text-white"
+        pageClassName="inline-block"
+        activeClassName="inline-block"
+        breakLabel="..."
+        pageRangeDisplayed={5}
+        renderOnZeroPageCount={null}
+      />
     </div>
   );
 };
+
